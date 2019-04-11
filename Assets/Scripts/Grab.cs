67071b0cb8f7f8ecaf17d4981b80be7ab0470397
Lauren <44094421+s011937h@users.noreply.github.com>
2019-04-11 17:46:12 +0100

    Scene setup

---
+++ Assets/Scripts/Grab.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using Valve.VR;
+
+public class Grab : MonoBehaviour
+{
+    private SteamVR_Input_Sources hand;
+
+    void Start()
+    {
+        var pose = GetComponent<SteamVR_Behaviour_Pose>();
+        hand = pose.inputSource;
+    }
+
+    /*void OnTriggerStay(Collider other)
+    {
+        Debug.Log("OnCollisionStay()");
+        // if trigger down 
+        if (GrabPinch(hand))
+        {
+            Transform grabbedTransform = other.transform;
+
+            // set the parent of the grabbed object to be the controller; 
+            // then the object will follow the controller
+            grabbedTransform.parent = transform;
+        }
+    }*/
+}
